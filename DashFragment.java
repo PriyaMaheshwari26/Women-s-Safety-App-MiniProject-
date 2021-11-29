341f00e11b7a19a93769c4e1077e3c269564b38a
Priya Maheshwari <82410835+PriyaMaheshwari26@users.noreply.github.com>
2021-11-28 21:44:57 -0800

    Add files via upload

---
+++ DashFragment.java
@@ -0,0 +1,26 @@
+package com.example.bsafeapp.ui.dashboard;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+
+import com.example.bsafeapp.R;
+
+
+public class DashFragment extends Fragment {
+    GridLayout gridLayout;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dash, container, false);
+
+
+    }
+}
